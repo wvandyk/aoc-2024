1f4f9eae813c479ba594a563046c1e6fc7fca77d
Wynand van Dyk <wvandyk@gmail.com>
2024-12-08 13:58:33 +0200

    Refactored and cleaned up day3 code.

--- src/day3_2024.cpp
+++ src/day3_2024.cpp
@@ -5,35 +5,27 @@ void day3_2024(void) {
     int day3_p2_total = 0;
 
     char *buffer = (char *)get_file_content("assets/day3_2024.txt");
-    int left_list[1000] = {};
-    int right_list[1000] = {};
-    int j = 0;
+    int left = 0;
+    int right = 0;
     int length = strlen(buffer);
     bool enable_mul = true;
 
     for(int i = 0; i < length; i++) {
-        char *b = &buffer[i];
         int n = 0;
-        int n2 = 0;
 
-        sscanf(b, "do()%n", &n2);
-        if(n2 > 0) {
+        if(strncmp(&buffer[i], "do()", 4) == 0) {
             enable_mul = true;
         }
-
-        n2 = 0;
-        sscanf(b, "don't()%n", &n2);
-        if(n2 > 0) {
+        if(strncmp(&buffer[i], "don't()", 7) == 0) {
             enable_mul = false;
         }
 
-        int res = sscanf(b, "mul(%3d,%3d)%n", &left_list[j], &right_list[j], &n);
+        int res = sscanf(&buffer[i], "mul(%3d,%3d)%n", &left, &right, &n);
         if((res == 2) && (n != 0)) {
-            day3_p1_total += left_list[j] * right_list[j];
+            day3_p1_total += left * right;
             if(enable_mul == true) {
-                day3_p2_total += left_list[j] * right_list[j];
+                day3_p2_total += left * right;
             }
-            j++;
         }
     }
     free(buffer);
